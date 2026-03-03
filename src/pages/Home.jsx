e54404a214a113864d3ec71a6db48cf1dcaeb3ab
Abdulaziz1214 <yormatov3@gmail.com>
2026-03-03 15:46:06 +0500

    Rasm dobavil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Bu HOMEEEEEEEE</h1>
+      <img src="https://i.pinimg.com/736x/3d/04/c8/3d04c85fd7ac7470db4e9f7b8aaa745b.jpg" alt="" />
     </div>
   )
 }
